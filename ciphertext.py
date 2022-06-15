c762673b74fd4c73794a5b9d94567def7e7e712f
Hoseung Choi <hopung@gmail.com>
2022-06-15 12:23:55 +0900

    np.ndarray check bug fix

--- ciphertext.py
+++ ciphertext.py
@@ -2,18 +2,18 @@ import numpy as np
 from cipher import key_hash
 
 class Ciphertext():
-    def __init__(self):
+    def __init__(self, logp, logq, nslots):
         """Base class for any ciphertext
         Currently following the CKKS convention -> should be changed.
         """
-        self.nslots = None
-        self.logp = None
-        self.logq = None
+        self.logp = logp
+        self.logq = logq
+        self.nslots = nslots
         self.level = 0
         pass
 
 class CiphertextStat(Ciphertext):
-    def __init__(self, arr):
+    def __init__(self, arr, *args, **kwargs):
         """class
 
         min, max: 
@@ -29,22 +29,24 @@ class CiphertextStat(Ciphertext):
             calculating virtually all statistical quantities.
             We assume that this value is given. 
         """
-        super().__init__()
+        super().__init__(*args, **kwargs)
         self._min = None  
         self._max = None
         self._mean = None
-        self._arr = arr
+        self._set_arr(arr)
         self._n_elements = self._arr.__len__()
         self._encrypted = False
         self._enckey_hash = None
     
     def _set_arr(self, arr):
-        if not isinstance(arr, np.ndarray) or not np.issubdtype(arr, np.number):
-            try:
-                arr = np.array(arr)
-            except:
-                print("Need numeric np.ndarray")
+        if not isinstance(arr, np.ndarray):
+            arr = np.array(arr)
 
+        if not np.issubdtype(arr.dtype, np.number):
+            print("Need a numeric type")
+            raise ValueError
+        else:
+            self._arr = arr
 
     def _encrypt(self, encrypt_key):
         """to be called by Encryptor
