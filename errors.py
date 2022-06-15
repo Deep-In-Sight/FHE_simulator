b87e7a5026972f7cc576358fe2cb5095c73c4ffd
Hoseung Choi <hopung@gmail.com>
2022-06-15 11:40:13 +0900

    Exception factory

---
+++ errors.py
@@ -0,0 +1,14 @@
+class CustomException(Exception):
+    default_message = ""
+    def __init__(self, *args, **kwargs):
+        if not args: args = (self.default_message,)
+        super().__init__(*args, **kwargs)        
+
+class ScaleMisMatchError(CustomException):
+    default_message = "Scales of Ctxts don't match"
+
+class DepthExhaustionError(Exception):
+    default_message = """No more multiplication is possible. Bootstrap needed"""
+
+class ErrorOutOfBoudError(Exception):
+    default_message = """Error has grown too large"""
