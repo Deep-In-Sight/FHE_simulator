d75b717d9872ca7cad5a1b2f320c6b3e85eeb120
Hoseung Choi <hschoi@dinsight.ai>
2022-10-30 17:32:40 +0900

    rename files and New Appr Sign and ReLU class

--- hemul/comparator.py
+++ hemul/comparator.py
@@ -85,3 +85,57 @@ def appr_relu(xin, *args, **kwargs):
     out = appr_sign(xin, *args, **kwargs)
 
     return xin * (out+1)/2
+
+class ApprSign():
+    def __init__(self, 
+                alpha=12, 
+                margin = 0.01, 
+                eps=0.02, 
+                min_depth=True, 
+                min_mult=False):
+        self.alpha = alpha
+        self.margin = margin
+        self.eps = eps
+        self.min_depth = min_depth
+        self.min_mult = min_mult
+        self.funs = None
+        self.degrees = None
+        if self.alpha is not None:
+            self._set_degree()
+        if self._params_set():
+            self._set_funs()
+
+    def _params_set(self):
+        return self.degrees is not None and self.margin is not None and self.eps is not None
+
+    def _set_degree(self):
+        if self.min_depth:
+            self.degrees = MINIMUM_DEPTH[self.alpha]
+        elif self.min_mult:
+            self.degrees = MINIMUM_MULT[self.alpha]
+    
+    def _set_funs(self, degrees=None):
+        if degrees is None:
+            degrees = self.degrees
+        self.funs = _appr_sign_funs(degrees, margin=self.margin, eps=self.eps)
+        print("functions set")
+        print(f"degrees = {self.degrees}, margin = {self.margin}, eps = {self.eps}") 
+
+    def __call__(self, xin):
+        if self.funs is not None:
+            for fun in self.funs:
+                xin = fun(xin)
+            return xin
+        else:
+            self._set_funs()
+            return self.__call__(xin)
+
+class ApprRelu(ApprSign):
+    def __init__(self):
+        super().__init__()
+    
+    def __call__(self, xin):
+        out = ApprSign.__call__(self, xin)
+        return xin * (out+1)/2
+
+        
